02aa5b671f7192c5a6ae162fa7e0dbcd143a5461
jesrammar <jesrammar2@alumn.us.es>
2025-08-29 16:56:58 +0100

    init: base project with Spring Boot entrypoint

---
+++ src/main/java/com/jesus/dicebearproxy/App.java
@@ -0,0 +1,8 @@
+package main.java.com.jesus.dicebearproxy;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
